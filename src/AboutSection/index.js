bc26236decc49890229f322c5179fadd98e80254
Nirushana <kavinda.n.t@gmail.com>
2021-08-13 01:49:36 +0530

    AboutSection

--- src/AboutSection/index.js
+++ src/AboutSection/index.js
@@ -26,7 +26,7 @@ function About() {
               Digital Dream a business reality. Get together with us today make your idea a reality. So Dream Big make it Big  
             </p>
             <AboutButton>
-            <a href="/" class="primary-btn">See My work</a>
+            <a href="/" class="primary-btn">Contact Us</a>
                 <a href="/">
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1" d="M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z" />
